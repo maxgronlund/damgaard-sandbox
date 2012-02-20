7ccdf4d894bb9bcc92b87f3f21c4c9692b12e052
Synthmax <max@synthmax.dk>
2012-02-20 12:27:15 +0100

    A nev company_controller for master JOE :-D

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,15 +2,23 @@ class CompaniesController < InheritedResources::Base
   #layout 'frontend'
   
   def show
-    @company = Company.find(params[:id])
-    @title = session[:locale] == 'dk' ? @company.title : @company.title_uk
-    @headline = session[:locale] == 'dk' ? @company.headline : @company.headline_uk
-    @body = session[:locale] == 'dk' ? @company.body : @company.body_uk
-    render :layout => 'frontend'
+    if Company.nil?
+      redirect_to admin_index_path
+    else 
+      @company = Company.find(params[:id])
+      @title = session[:locale] == 'dk' ? @company.title : @company.title_uk
+      @headline = session[:locale] == 'dk' ? @company.headline : @company.headline_uk
+      @body = session[:locale] == 'dk' ? @company.body : @company.body_uk
+      render :layout => 'frontend'
+    end
   end
   
   def update
     update!{ admin_company_path(@company)}
+  end
+  
+  def destroy
+    destroy!{ admin_index_path }
   end
 
 end
