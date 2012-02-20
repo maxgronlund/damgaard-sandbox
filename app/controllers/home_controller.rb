6ba3b1e6d5218f25e2d5ede39b9f624c409b234c
Synthmax <max@synthmax.dk>
2012-02-20 13:17:23 +0100

    fall back that works as intended

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   
   layout 'frontend'
+  
+  def index
+    if Company.all.empty?
+      redirect_to admin_index_path
+    else
+      redirect_to company_path(Company.first.id)
+    end
+      
+  end
 
 end
